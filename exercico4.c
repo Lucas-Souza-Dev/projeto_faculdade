a10d0fc9024614e959bdc2f757e51a62a239f323
Lucas Pereira <71991303+Lucas-Souza-Dev@users.noreply.github.com>
2021-11-28 13:58:14 -0300

    Update exercico4.c
    Correção e continuidade do sistema condicional do codigo

--- exercico4.c
+++ exercico4.c
@@ -7,7 +7,10 @@ printf("Informe sua idade para saber se você é de maior: \n");
 scanf("%d",idade);
 
 if(idade >= 18){
-    printf("")
+    printf("Você é de maior");
+    return 0;
 };
+    
+printf("Você é de menor");
 return 0
 }
